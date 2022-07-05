9427e9160a2db0fd8c3ac4b4990c690460c4b7ba
Abhishek Potdar <abhirpotdar@gmail.com>
2022-07-06 00:27:24 +0530

    Updated Config URL

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Africa/Gaborone');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:1010/ticket-management-system';
+$config['base_url'] = 'http://maridd-tms.purecss.co.in';
 
 /*
 |--------------------------------------------------------------------------
